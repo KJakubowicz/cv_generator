e543e8e1a40ebacdf38bcfbf4ee291de8ac7d329
KJakubowicz <kjakubowicz98@interia.pl>
2023-03-22 19:55:00 +0100

    Make controller for generator

---
+++ app/src/Controller/GeneratorController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GeneratorController extends AbstractController
+{
+    #[Route('/generator', name: 'app_generator')]
+    public function index(): Response
+    {
+        return $this->render('generator/index.html.twig', [
+            'controller_name' => 'GeneratorController',
+        ]);
+    }
+}
