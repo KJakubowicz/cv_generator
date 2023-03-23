77058276b1bc6d20c3d7505a4e01cde7c40ad45b
KJakubowicz <kjakubowicz98@interia.pl>
2023-03-23 22:16:28 +0100

    Added validator and generator object

--- app/src/Generators/Generator.php
+++ app/src/Generators/Generator.php
@@ -2,9 +2,13 @@
 
 namespace App\Generators;
 
+use App\Object\GeneratorObject;
+use App\Validator\Validator;
 
 abstract class Generator
 {
+    protected GeneratorObject $object;
+    protected Validator $validator;
     protected $data = [];
 
     abstract public function generate(array $params): void;
